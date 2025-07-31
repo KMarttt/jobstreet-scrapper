e508dbb7e6f2d451691316914bb05eba3db89c23
Marti Trance <martitrance@gmail.com>
2025-07-31 23:01:21 +0800

    Added column for work_setup, job_function, listing_type

--- job_street_scraper.py
+++ job_street_scraper.py
@@ -6,6 +6,7 @@ from numpy import nan
 from datetime import datetime, timedelta
 import re
 
+
 async def parse_date_posted(page):
     date_text = (await page.locator("xpath=(//span[contains(text(),'Posted')])[1]").first.text_content()).strip()
 
@@ -13,12 +14,31 @@ async def parse_date_posted(page):
     relative_date_text = date_text.replace("Posted", "").replace("ago", "").strip()
 
     if "d" in relative_date_text: # if the date is in days
-        days = int(relative_date_text.replace("d", "").strip())
+        # days = int(relative_date_text.replace("d", "").replace("+", "").strip())
+        days = int(re.sub(r"[^\d]", "", relative_date_text))
         return (current_date - timedelta(days=days)).strftime(r"%Y-%m-%d")
     elif "h" in relative_date_text: # If the date is in hour(s) ago
         return current_date.strftime(r"%Y-%m-%d")
     else: # If the date is in minute(s) or second(s)
         return current_date.strftime(r"%Y-%m-%d")
+    
+async def parse_location(page):
+    location_section = (await page.locator("span[data-automation='job-detail-location']").text_content()).strip()
+
+    if re.search("(hybrid)", location_section.lower()):
+        location = location_section.split("(hybrid)")[0].strip()
+        work_setup = "hybrid"
+        is_remote = False
+    elif re.search("(remote)", location_section.lower()):
+        location = location_section.split("(remote)")[0].strip()
+        work_setup = "remote"
+        is_remote = True
+    else:
+        location = location_section
+        work_setup = "onsite"
+        is_remote = False
+    
+    return location, is_remote, work_setup
 
 async def parse_salary(page, currency_values, currency_dictionary):
     salary_locator = page.locator("span[data-automation='job-detail-salary']")
@@ -47,7 +67,7 @@ async def parse_salary(page, currency_values, currency_dictionary):
         # Fallback if the currency of the country portal is not found
         if "currency" not in locals():
             for c in currency_dictionary:
-                if re.search(c.lower(), salary_text):
+                if c.lower() in salary_text:
                     currency = currency_dictionary[c]
                     break    
         
@@ -70,8 +90,6 @@ async def parse_salary(page, currency_values, currency_dictionary):
     return salary_source, interval, min_amount, max_amount, currency
 
 
-
-
 async def web_scraper(portal="my", location="", keyword="Data-Analyst", page_number=1):
     # Phase 1: Initiate
     print("Initiating JobStreet Scraper")
@@ -83,20 +101,19 @@ async def web_scraper(portal="my", location="", keyword="Data-Analyst", page_num
 
         # Configuring the browser to be Incognito (to have clean cookies, cache, etc.)
         context = await browser.new_context()
-
-
+        # Open new page
         page = await context.new_page()
         job_links = []
         job_data = []
 
-        # # Block unnecessary resources
-        # async def route_handler(route, request):
-        #     if request.resource_type in ["image", "stylesheet", "font"]:
-        #         await route.abort()
-        #     else:
-        #         await route.continue_()
+        # Block unnecessary resources
+        async def route_handler(route, request):
+            if request.resource_type in ["image", "stylesheet", "font"]:
+                await route.abort()
+            else:
+                await route.continue_()
         
-        # await context.route("**/*", route_handler)
+        await context.route("**/*", route_handler)
 
         # , wait_until="networkidle"
 
@@ -122,18 +139,17 @@ async def web_scraper(portal="my", location="", keyword="Data-Analyst", page_num
         print(f"Number of Job links collected: {len(job_links)}")
 
         currency_country_dictionary = {
-            "ph": ["₱", "PHP"],
-            "th": ["฿", "THB"],
-            "my": ["RM", "MYR"],
-            "id": ["Rp", "IDR"]
+            "ph": ["PHP", "₱"],
+            "th": ["THB", "฿"],
+            "my": ["MYR", "RM"],
+            "id": ["IDR", "Rp", ]
         }
         currency_values = currency_country_dictionary[portal]
 
         currency_dictionary = {
-            "IDR": "IDR", "MYR": "MYR",  "PHP": "PHP", "THB": "THB", "USD": "USD", "SGD": "SGD", "VND": "VND", 
+            "IDR": "IDR", "MYR": "MYR", "PHP": "PHP", "THB": "THB", "USD": "USD", "SGD": "SGD", "VND": "VND", 
             "Rp": "IDR", "RM": "MYR", "₱": "PHP", "฿": "THB", "$": "SGD", "S$": "SGD", "₫": "VND",   
         }
-
 
 
         # Phase 3: Extract Job Details
@@ -144,57 +160,41 @@ async def web_scraper(portal="my", location="", keyword="Data-Analyst", page_num
             site = "jobstreet"
             job_url = f"https://{portal}.jobstreet.com{link}" 
             print(job_url)
-            job_url_direct = nan
-
             await page.goto(job_url)
 
-            title = (await page.locator("h1[data-automation='job-detail-title']").text_content()).strip()
-
+            title = (await page.locator("span[data-automation='job-detail-location']").text_content()).strip()
             company = (await page.locator("span[data-automation='advertiser-name']").text_content()).strip()
-            location = (await page.locator("span[data-automation='job-detail-location']").text_content()).strip()
+            location, is_remote, work_setup = await parse_location(page)
             date_posted = await parse_date_posted(page)
-            
-
-
-            
-            
-            
             job_type = (await page.locator("span[data-automation='job-detail-work-type']").text_content()).strip()
-            # salary_locator = page.locator("span[data-automation='job-detail-salary']")
             salary_source, interval, min_amount, max_amount, currency= await parse_salary(page, currency_values, currency_dictionary)
-
-
-            
-
             job_function = (await page.locator("span[data-automation='job-detail-classifications']").text_content()).strip()
             job_description = (await page.locator("div._1lns5ab0.sye2ly0").text_content()).strip()
+            listing_type = link.split("type=")[1].split("&")[0]
 
             job_data.append({
                 "id": job_id,
                 "site": site,
-                "job_link": job_url,
+                "job_url": job_url,
+                "job_url_direct": nan,
                 "title": title,
                 "company": company,
                 "location": location,
                 "date_posted": date_posted,
-                "job_function": job_function,
                 "job_type": job_type,
                 "salary_source": salary_source,
                 "interval": interval,
                 "min_amount": min_amount,
                 "max_amount": max_amount,
                 "currency": currency,
-                "job_description_raw": job_description,
+                "is_remote": is_remote,
+                "work_setup": work_setup,
+                "job_level": nan,
+                "job_function": job_function,
+                "listing_type": listing_type,
+                "emails": nan,
+                "description": job_description,
             })
-
-            # print(job_position)
-            # print(company_name)
-            # print(location)
-            # print(date_posted)
-            # print(department)
-            # print(employment_type)
-            # print(salary_range)
-            # print(job_description)
         
         print("Extraction Completed")
         print("Saving data to CSV")
@@ -204,12 +204,10 @@ async def web_scraper(portal="my", location="", keyword="Data-Analyst", page_num
             if data_frame[col].dtype == "object":
                 data_frame[col] = data_frame[col].str.replace("\n", " ", regex=False).str.replace("\r", " ", regex=False)
 
-
         data_frame.to_csv("data/jobstreet_jobs.csv", index=False, quotechar='"', escapechar='\\', encoding='utf-8-sig')
         print("Data saved to CSV")
 
 # Run the Function
-
 if __name__ == "__main__":
 
     # User Input
@@ -227,5 +225,5 @@ if __name__ == "__main__":
     # Test Run
     asyncio.run(web_scraper())
 
-    # Proper Run
+    # # Proper Run
     # asyncio.run(web_scraper(portal, location, keyword, page_number))
